7894068bf8fdc13adc65a87fafabe1df70f38686
nathanellodar <117901115+nathanellodar@users.noreply.github.com>
2025-04-02 20:34:24 +0700

    persiapan project
    kurang node modules

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Home() {
+  return (
+    <div className='flex place-content-center'>
+      <h2>Hello</h2>
+    </div>
+  )
+}
+
+export default Home
