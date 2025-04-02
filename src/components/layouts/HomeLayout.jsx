df38719e0fcdd19a262dccb57e1485066cf37430
nathanellodar <117901115+nathanellodar@users.noreply.github.com>
2025-04-02 20:43:44 +0700

    ubah struktur file
    memindahkan folder layouts ke dalam components dan menambahkan folder modules ke components folder

--- src/components/layouts/HomeLayout.jsx
+++ src/components/layouts/HomeLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Home from '../../pages/Home'
 
 const HomeLayout = () => {
     return (
-        <div>
-
-        </div>
+        <main>
+            <Home/>
+        </main>
     )
 }
 
